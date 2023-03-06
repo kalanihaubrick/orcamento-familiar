e1ab5414b457bc31f050d3e6652a033432e8f973
kalanihaubrick <kalanihm@yahoo.com.br>
2023-03-05 21:04:48 -0300

    Feito o restante do crud de despesas.

--- api/controller/DespesasController.js
+++ api/controller/DespesasController.js
@@ -30,6 +30,62 @@ class DespesasController {
             return res.status(500).json(error.message)
         }
     }
+
+    static async consultaDespesas(req, res) {
+        try {
+            const todasDespesas = await despesasService.pegaTodosRegistros({ attributes: ['descricao', 'valor', 'data'] })
+            return res.status(200).json(todasDespesas)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async consultaUmaDespesa(req, res) {
+        const { id } = req.params
+        try {
+            const umaDespesa = await despesasService.pegaUmRegistro({id : id})
+            if (umaDespesa == null) {
+                res.status(404).json({ message: "Não existe despesa com este ID" })
+            } else {
+                return res.status(200).json(umaDespesa)
+            }
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async atualizaDespesa(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+        try {
+            await despesasService.atualizaRegistro(novasInfos, id)
+            const despesaAtualizada = await despesasService.pegaUmRegistro({id : id})
+            if (despesaAtualizada == null) {
+                res.status(404).json({ message: "Não existe despesa com este ID" })
+            } else {
+                return res.status(200).json(despesaAtualizada)
+            }
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async apagaDespesa(req, res) {
+        const { id } = req.params
+
+        try {
+            const despesa = await despesasService.pegaUmRegistro({id : id})
+
+            if (despesa == null) {
+                res.status(404).json({ message: "Não existe despesa com este ID" })
+            } else {
+                await despesasService.apagaRegistro({id: id})
+                res.status(200).json({ message: `Despesas de ID ${id} apagado.` })
+            }
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = DespesasController;
--- api/routes/despesasRoutes.js
+++ api/routes/despesasRoutes.js
@@ -6,5 +6,10 @@ const router = express.Router()
 router
     .post('/despesas', DespesasController.criaDespesa)
 
+    .get('/despesas', DespesasController.consultaDespesas)
+    .get('/despesas/:id', DespesasController.consultaUmaDespesa)
 
+    .put('/despesas/:id', DespesasController.atualizaDespesa)
+
+    .delete('/despesas/:id', DespesasController.apagaDespesa)
     module.exports = router;
